224148d0202182a3d417c64e32ad6e59a8962a04
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2022-10-06 17:56:00 +0100

    Update getGenres.ts

--- src/lib/server/getGenres.ts
+++ src/lib/server/getGenres.ts
@@ -3,29 +3,19 @@ import { API_KEY } from '$env/static/private';
 const GENRES_TV_API = `https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}&language-en-GB`;
 const GENRES_MOVIE_API = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language-en-GB`;
 
-const fetch_tv = async (): Promise<Genre[]> => {
-    try {
-        const response_tv = await fetch(GENRES_TV_API)
-        const tv_genre = await (response_tv.json())
-        return tv_genre.genres
-    }
-    catch {
-        throw error(404, 'network error, genres request failed')
-    }
+export const tv_genre = () => {
+
+    fetch(GENRES_TV_API)
+        .then(resp => resp.json())
+        .then(tv_genre => tv_genre.genres)
+    return tv_genre
 }
 
-const fetch_movie = async (): Promise<Genre[]> => {
-    try {
-        const response_movie = await fetch(GENRES_MOVIE_API)
-        const movie_genre = await (response_movie.json())
-        return movie_genre.genres
-    }
-    catch {
-        throw error(404, 'no network')
-    }
+export const movie_genre = () => {
+
+    fetch(GENRES_MOVIE_API)
+        .then(resp => resp.json())
+        .then(movie_genre => movie_genre.genres)
+    return movie_genre
+
 }
-
-const tv_genre = await fetch_tv()
-const movie_genre = await fetch_movie()
-export { tv_genre, movie_genre }
-
