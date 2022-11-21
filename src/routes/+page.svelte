28e66f493e35a70a2efcf5858cc6288c477b9da6
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2022-11-21 02:35:52 +0000

    added responsive region change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import MainSection from '$lib/pages/MainSection.svelte';
+	import { page } from '$app/stores';
 	import { selected } from '$lib/stores/store';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	const { media_data, total_pages } = data;
-
 	$selected = null;
 </script>
 
-<MainSection {media_data} {total_pages} media_type={'all'} />
+{#key $page.url}
+	<MainSection media_data={data.media_data} total_pages={data.total_pages} media_type={'all'} />
+{/key}
